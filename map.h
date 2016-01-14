0e5b4dcfd2f002d8c301e13d522785ae08951fa6
Tlospock <meunierpaul@free.fr>
2016-01-14 20:48:21 +0100

    Update map.h

--- map.h
+++ map.h
@@ -1,1 +1,16 @@
+#ifndef _MAP_H
+#define _MAP_H
+
+#include "structure.h"
+
+#define IMINDESTRU 1
+#define IMAXDESTRU 2
+
+#define HAUTEUR_MAP 50      /* Multiple de 3 ou 4 */
+#define LONGUEUR_MAP 50     /* Multiple de 2 */
+
+void init_map(int** carte, int longueur, int largeur);
+
+
+#endif // _MAP_H
 
