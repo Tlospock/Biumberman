2385318bdb5970a5b2f1accea3e17465d4739220
Tlospock <meunierpaul@free.fr>
2016-01-14 20:46:58 +0100

    Create map.c

---
+++ map.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "map.h"
+
+void init_map(int** carte, int longueur, int largeur) /*Prend en paramètre un pointeur sur un tableau à deux dimensions et le rempli*/
+{
+    /*
+        Generation blocs indestructibles
+        Generation blocs air (ne pas oublier les position de départ)
+        Completer avec des blocs destructibles
+    */
+    
+    int i, j;
+    int quart = longueur*largeur/4, demi = longueur*largeur/2;
+    
+    /* Remplissage de la carte avec que des blocs destructibles */
+    
+    for(i=0; i<longueur;++i)
+    {
+        for(j=0;j<largeur;++j)
+            carte[i][j]=(rand() % (IMAXDESTRU - IMINDESTRU + 1)) + IMINDESTRU;
+    }
+    
+    /* Création des blocs indestructibles: random */
+    
+    
+    
+    /* Création des blocs air: random*/
+    
+    /* Création des blocs fixes qui doivent toujours être là*/
+    
+}
