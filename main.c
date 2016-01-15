8fe7e28a94c2ad0a41db5f43a1a35754d74ae86a
Tlospock <meunierpaul@free.fr>
2016-01-15 17:06:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "map.h"
+#include "affichage.h"
 
 int main()
 {
     srand(time(NULL));
 
     printf("Hello world!\n");
+
+    int var=1;
+    var = test();
+    printf("var = %d", var);
+
     return 0;
 }
