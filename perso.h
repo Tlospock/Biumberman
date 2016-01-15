8d124e523c187535860e92d071767b531a7cbd57
Tlospock <meunierpaul@free.fr>
2016-01-15 17:04:08 +0100

    Update perso.h

--- perso.h
+++ perso.h
@@ -4,5 +4,30 @@
 #include "map.h"
 
 
+typedef struct perso{
+    short int id;
+    Position pos;
+    short int vie;
 
-#endif // _PERSO_H
+    short int nbBombeTot;
+    short int nbBombePos;
+
+    short int poussee;
+    int vitesse;
+    int radius;
+    short int effetBonus;
+
+}Perso;
+
+/* Numero d'identification des effets bonus qui ne modifient pas les caracs
+ * 0 Rien
+ * 1 Feu
+ * 2 Glace
+ * 3 Mine
+ */
+
+void init_perso(Square **carte, Perso* tab_perso, int nb_perso);
+void deplacer(int idJoueur);
+void poseBombe(Square** carte, Perso* idJoueur);
+void exploser(Square** carte, Position posBombe);
+#endif /* _PERSO_H*/
