e47ef0ba19b7dd04760e47ad2ca8dabbb3af6802
Tlospock <meunierpaul@free.fr>
2016-01-15 17:14:43 +0100

    Update map.c

--- map.c
+++ map.c
@@ -5,32 +5,57 @@
 
 #include "map.h"
 
-void init_map(int** carte, int longueur, int largeur) /*Prend en paramètre un pointeur sur un tableau à deux dimensions et le rempli*/
+void init_map(Square** carte, int longueur, int hauteur)
 {
-    /*
-        Generation blocs indestructibles
-        Generation blocs air (ne pas oublier les position de départ)
-        Completer avec des blocs destructibles
-    */
-    
-    int i, j;
-    int quart = longueur*largeur/4, demi = longueur*largeur/2;
-    
-    /* Remplissage de la carte avec que des blocs destructibles */
-    
-    for(i=0; i<longueur;++i)
-    {
-        for(j=0;j<largeur;++j)
-            carte[i][j]=(rand() % (IMAXDESTRU - IMINDESTRU + 1)) + IMINDESTRU;
-    }
-    
-    /* Création des blocs indestructibles: random */
-    
-    
-    
-    /* Création des blocs air: random*/
-    
-    /* Création des blocs indestru/air fixe qui doivent toujours être là: position de départ, centre de la map*/
-    
+	int i, j, x, y;
+	
+	for(i=0; i<hauteur; ++i)
+	{
+		for(j=0; j<longueur;++j)
+			carte[i][j].bloc.type=(rand() % (IMAXDESTRU - IMINDESTRU +1)) + IMINDESTRU;
+	}	
+	/* Creation de blocs indestructibles */
+	for(i=0; i<hauteur-1; ++i)
+	{
+		for(j=1;j<longueur-1;++j)
+		{
+			if(i>hauteur-1 && carte[j-1][i+1].bloc.type != -1 && carte[j][i+1].bloc.type != -1 && carte[j+1][i+1].bloc.type != -1)
+			{
+				if(i != 0 && carte[j-1][i-1].bloc.type != -1 && carte[j][i-1].bloc.type != -1 && carte[j+1][i-1].bloc.type != -1 && carte[j-1][i].bloc.type != -1 && carte[j+1][i].bloc.type != -1)
+					carte[j][i].bloc.type = -1;
+				else if(i == 0)
+					carte[j][i].bloc.type = -1;
+			}
+		}
+		++i;
+		for(j=0; j<longueur; j=j+2)
+		{
+			x=rand() % (2);
+			y=rand() % (2);
+			carte[j+y][i+x].bloc.type = -1;
+		}	
+	}
+	
+	/* Creation des blocs d'airs*/
+	/*Creation bloc d'air obligatoire*/
+	carte[1][1].bloc.type = carte[1][2].bloc.type = carte[2][1].bloc.type = 0; /*Placement joueur en haut à gauche*/
+	carte[longueur-2][1].bloc.type = carte[longueur-2][2].bloc.type = carte[longueur-3][1].bloc.type = 0; /*Placement joueur en haut à droite*/
+	carte[1][hauteur-2].bloc.type = carte[hauteur-2][2].bloc.type = carte[hauteur-3][1].bloc.type = 0; /*Placement joueur en bas à gauche*/
+	carte[longueur-2][hauteur-2].bloc.type = carte[longueur-2][hauteur-3].bloc.type = carte[longueur-3][hauteur-2].bloc.type = 0; /*Placement joueur en haut à gauche*/
+	
+	/*Création de blocs airs aléatoires (moins du quart de total des blocs)*/
+	int nbBlocAir = longueur*hauteur/5;
+	int compteur = nbBlocAir;
+	while(compteur < 0)
+	{
+		i=rand() % longueur;
+		j=rand() % hauteur;
+		
+		if(carte[i][j].bloc.type >0)
+		{
+			carte[i][j].bloc.type = 0;
+			compteur--;
+		}
+	}
 }
 
