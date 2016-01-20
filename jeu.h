20c06e74bed4c853dd13af4f6d1088018ef6d909
Biunoctium <Biunoctium@users.noreply.github.com>
2016-01-20 01:10:07 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,7 +1,15 @@
 #ifndef _JEU_H
 #define _JEU_H
 
+#include "affichage.h"
+
+/*retourne le nb de joueurs*/
+int menu_joueurs(SDL_Window* window, SDL_Surface* screenSurface);
+/*retourne la taille de map choisie*/
+int menu_taille();
+
 int jeu();
+
 
 #endif /* _JEU_H*/
 
